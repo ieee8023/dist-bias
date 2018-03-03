e678ff4d6a34187a00fcfff4416d625867e22334
Joseph Paul Cohen <joseph@josephpcohen.com>
2018-03-03 02:20:56 -0500

    classifier code

---
+++ cyclegan/run-transform-classifiertrainingdata.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+RATIO=$1
+
+python test.py --dataroot "./datasets/brats2013-var/brats2013_0.5" --model "cycle_gan" --name "brats2013_cyclegan_$RATIO" --checkpoints_dir "./checkpoints" --input_nc 1 --output_nc 1 --resize_or_crop "scale_width" --how_many 10000
+
+
+#--continue_train
--- cyclegan/train.py
+++ cyclegan/train.py
@@ -2,7 +2,7 @@ import time
 from options.train_options import TrainOptions
 from data.data_loader import CreateDataLoader
 from models.models import create_model
-from util.visualizer import Visualizer
+#from util.visualizer import Visualizer
 
 opt = TrainOptions().parse()
 data_loader = CreateDataLoader(opt)
@@ -11,7 +11,7 @@ dataset_size = len(data_loader)
 print('#training images = %d' % dataset_size)
 
 model = create_model(opt)
-visualizer = Visualizer(opt)
+#visualizer = Visualizer(opt)
 total_steps = 0
 
 for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
@@ -20,7 +20,7 @@ for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
 
     for i, data in enumerate(dataset):
         iter_start_time = time.time()
-        visualizer.reset()
+        #visualizer.reset()
         total_steps += opt.batchSize
         epoch_iter += opt.batchSize
         model.set_input(data)
@@ -28,14 +28,14 @@ for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
 
         if total_steps % opt.display_freq == 0:
             save_result = total_steps % opt.update_html_freq == 0
-            visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+            #visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
         if total_steps % opt.print_freq == 0:
             errors = model.get_current_errors()
             t = (time.time() - iter_start_time) / opt.batchSize
-            visualizer.print_current_errors(epoch, epoch_iter, errors, t)
-            if opt.display_id > 0:
-                visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
+            #visualizer.print_current_errors(epoch, epoch_iter, errors, t)
+            #if opt.display_id > 0:
+            #    visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
 
         if total_steps % opt.save_latest_freq == 0:
             print('saving the latest model (epoch %d, total_steps %d)' %
